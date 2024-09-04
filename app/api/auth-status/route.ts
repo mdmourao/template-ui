d34c4466950f0221274ee7ebf81b2dfb8f8c2f8b
mdmourao <martimmourao@gmail.com>
2024-09-04 14:21:13 +0100

    Started implementing auth logic

---
+++ app/api/auth-status/route.ts
@@ -0,0 +1,9 @@
+export async function GET(req: Request) {
+    const resp = await fetch(`http://localhost:7777/auth-status`, { method: "POST" })
+    try {
+        const data = await resp.json()
+        return Response.json({ data }, { status: resp.status })
+    } catch (e) {
+        return Response.error
+    }
+}
--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,9 +1,15 @@
-export async function POST(req: Request) {
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request, res: Response) {
     const body = await req.json()
-    const resp = await fetch(`http://localhost:7777/login`, { method: "POST", body: JSON.stringify(body) })
+    const resp = await fetch(`http://localhost:7777/login`, { 
+        method: "POST",
+        body: JSON.stringify(body),
+        credentials: "same-origin" })
     try {
         const data = await resp.json()
-        return Response.json({ data }, { status: resp.status })
+        const  response = Response.json({ data }, { status: resp.status , headers: { 'set-cookie': resp.headers.get('set-cookie') || "" }})
+        return response
     } catch (e) {
         return Response.error
     }
